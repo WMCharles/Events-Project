60a3d36b807c454a1d56e06b7507c8ba3bb471fc
WMCharles <charles.wafula@student.moringaschool.com>
2023-01-10 09:40:25 +0300

    change class to className
    to fit react naming convention

--- src/components/HeroLikes.jsx
+++ src/components/HeroLikes.jsx
@@ -4,7 +4,7 @@ import '../css/Likes.css'
 export default function HeroLikes() {
     return (
         <div className="section">
-            <div class="welcome-message">
+            <div className="welcome-message">
                 <h1>Your Favorite Events</h1>
                 <p>Here are the events that you have liked on our website. Click on an event to view more details.</p>
             </div>

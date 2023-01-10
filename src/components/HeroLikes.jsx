767b9a76d0ba4f895aedf4ba8a04253dca2fad22
WMCharles <charles.wafula@student.moringaschool.com>
2023-01-10 09:29:55 +0300

    add HeroLikes component

---
+++ src/components/HeroLikes.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../css/Likes.css'
+
+export default function HeroLikes() {
+    return (
+        <div className="section">
+            <div class="welcome-message">
+                <h1>Your Favorite Events</h1>
+                <p>Here are the events that you have liked on our website. Click on an event to view more details.</p>
+            </div>
+        </div>
+    )
+}

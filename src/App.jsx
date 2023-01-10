1d181c6aaf16809256530c222c4998ca23c8c728
WMCharles <charles.wafula@student.moringaschool.com>
2023-01-10 08:53:04 +0300

    initial commits

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar';
+import Likes from './components/Likes';
+import Home from './components/Home';
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <NavBar/>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='products' element={<Likes />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;

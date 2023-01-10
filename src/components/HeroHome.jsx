1159c54e822aa48998fe3f6844c2bf7b861dc132
WMCharles <charles.wafula@student.moringaschool.com>
2023-01-10 09:15:29 +0300

    add HeroHome component

---
+++ src/components/HeroHome.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../css/HeroHome.css'
+
+export default function HeroHome() {
+    return (
+        <section>
+            <div class="welcome-msg">
+                <h1>Welcome to Event Plugs</h1>
+                <p>
+                    Discover amazing events happening in your city. From concerts and
+                    festivals to workshops and conferences, we've got you covered. Browse
+                    our calendar and start planning your next adventure today!
+                </p>
+            </div>
+        </section>
+    )
+}

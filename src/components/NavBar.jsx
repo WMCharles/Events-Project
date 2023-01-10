599e8640184752eeba8f50d1dbf026871f2214cd
WMCharles <charles.wafula@student.moringaschool.com>
2023-01-10 09:08:01 +0300

    add navbar structure

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,21 @@
+import { Link, Outlet } from 'react-router-dom'
 import React from 'react'
+import '../css/NavBar.css'
 
 export default function NavBar() {
-  return (
-    <div>
-        NavBar
-    </div>
-  )
+    return (
+        <nav>
+            <input type="checkbox" name="" id="check" />
+            <label htmlFor="check" className="checkbtn"><i className="fas fa-bars"></i></label>
+            <label className="logo">EVENT PLUGS</label>
+            <ul>
+                <li><Link href="index.html">Home</Link></li>
+                <li><Link href="#events-container">Events</Link></li>
+                <li><Link href="likes.html">Likes</Link></li>
+                <li><Link href="#">Create Event</Link></li>
+                <li><Link href="#">Logout</Link></li>
+            </ul>
+            <Outlet/>
+        </nav>
+    )
 }

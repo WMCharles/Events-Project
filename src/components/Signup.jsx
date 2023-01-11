d7f30df3c26a4f855e40655436bff398b317c8ce
WMCharles <charles.wafula@student.moringaschool.com>
2023-01-11 23:16:42 +0300

    add signup functionality

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,24 +2,37 @@ import React, { useState } from 'react';
 import '../css/Signup.css';
 // import { Link } from 'react-router-dom';
 
-function SignupForm({handleClick}) {
-  const [fullName, setFullName] = useState('');
+function SignupForm({ handleClick, onLogin }) {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // validate form fields
-    if (fullName.trim() === '' || email.trim() === '' || password.trim() === '' || confirmPassword.trim() === '') {
-      // display an error message
-      return;
-    }
-    if (password !== confirmPassword) {
-      // display an error message
-      return;
-    }
-    // submit form
+  function handleSubmit(e) {
+    e.preventDefault();
+    setErrors([]);
+    setIsLoading(true);
+    fetch("https://event-plug.onrender.com/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+        password_confirmation: passwordConfirmation,
+        role: "User"
+      }),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((user) => onLogin(user));
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
   }
 
   return (
@@ -29,25 +42,32 @@ function SignupForm({handleClick}) {
         <h3>Sign Up</h3>
         <label className='label'>
           Full Name
-          <input type="text" value={fullName} onChange={(event) => setFullName(event.target.value)} />
+          <input type="text" name="username" value={name} onChange={(e) => setName(e.target.value)} />
         </label>
         <br />
         <label className='label'>
           Email address
-          <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+          <input type="email" name="email" value={email} onChange={(event) => setEmail(event.target.value)} />
         </label>
         <br />
         <label className='label'>
           Password
-          <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+          <input type="password" name="password" value={password} onChange={(event) => setPassword(event.target.value)} />
         </label>
         <br />
         <label className='label'>
           Confirm Password
-          <input type="password" value={confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)} />
+          <input type="password" name="passwordConfirmation" value={passwordConfirmation} onChange={(event) => setPasswordConfirmation(event.target.value)} />
         </label>
         <br />
-        <button type="submit">Submit</button>
+        {errors.length > 0 && (
+          <div style={{ color: "red" }}>
+            {errors.map((error) => (
+              <p key={error}>{error}</p>
+            ))}
+          </div>
+        )}
+        <button type="submit">{isLoading ?  'Submiting..' : 'Submit'}</button>
         <p>Already have an account? <a href='login' onClick={handleClick}>Login</a></p>
       </form>
     </div>

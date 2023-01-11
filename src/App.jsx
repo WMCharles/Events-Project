85f4a0c7158e6aa46412666558f53da85430aff7
WMCharles <charles.wafula@student.moringaschool.com>
2023-01-11 22:45:39 +0300

    add logout functionality

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <NavBar />
+        <NavBar setUser={setUser}/>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/favorites' element={<Likes />} />

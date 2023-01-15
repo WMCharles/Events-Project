66b3319845f98bf0784037e31501aa222f75c3c2
Amos Langat <amos.langat@student.moringaschool.com>
2023-01-15 20:13:53 +0300

    add an event card

---
+++ src/components/EventCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+
+const EventCard = (props) => {
+    return (
+        <EventCardStyle className="eventCard">
+            <div className="image">
+                <img src={props.image} alt={props.name}/>
+            </div>
+            <div className="event-description">
+                <h3>{props.name}</h3>
+                <p className="start-date">Date: {props.start_date}</p>
+                <p>Location: {props.location}</p>
+                <p>Price: <span className="price-tag">{props.price}</span></p>
+            </div>
+        </EventCardStyle>
+    )
+}
+
+const EventCardStyle = styled.div`
+width: 350px;
+height: 400px;
+line-height: 2em;
+margin: 30px auto;
+padding: 10px;
+cursor: pointer;`
+
+export default EventCard;

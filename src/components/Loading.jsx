c5fd2a379373edd4e23bbb1df062ba81745936eb
WMCharles <charles.wafula@student.moringaschool.com>
2023-01-18 11:21:22 +0300

    add loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/Loading.css'
+
+export default function Loading() {
+    return (
+        <div className='loading'>
+            <h3>
+                loading...
+            </h3>
+        </div>
+    )
+}

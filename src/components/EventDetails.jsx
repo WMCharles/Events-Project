49892d3711764efcfb9f747b88e978ce9b7af3df
WMCharles <charles.wafula@student.moringaschool.com>
2023-01-18 11:20:40 +0300

    refactor event details page

--- src/components/EventDetails.jsx
+++ src/components/EventDetails.jsx
@@ -1,99 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import '../css/Details.css'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Loading from "./Loading";
+import EventItem from "./EventItem";
+import "../css/Details.css";
 
-export default function EventDetails({ addToLikes }) {
-
-    const { id } = useParams()
-    const [event, setEvent] = useState([])
-    const [loading, setLoading] = useState(true)
-    const url = `https://maps.google.com/maps?q=${event.location}&t=&z=13&ie=UTF8&iwloc=&output=embed`
+export default function EventDetails({ addToLikes, user }) {
     
-
-    // date formating
-    const date = new Date(event.start_date);
-    const event_date = date.toDateString()
-
-
+    // states
+    const { id } = useParams();
+    const [event, setEvent] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const url = `https://maps.google.com/maps?q=${event.location}&t=&z=13&ie=UTF8&iwloc=&output=embed`;
 
     const fetchProduct = async () => {
         fetch(`https://event-plug.onrender.com/events/${id}`)
             .then((res) => res.json())
             .then((item) => {
-                setLoading(false)
-                setEvent(item)
-                // console.log(item)
-            })
-    }
-    useEffect(() => {
-        fetchProduct()
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+                setLoading(false);
+                setEvent(item);
+            });
+    };
 
+    useEffect(() => {
+        fetchProduct();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     function handleAddToLikes(item) {
-        // console.log(item.id)
-        addToLikes(item)
+        addToLikes(item);
     }
 
     return (
-        <div className="Details">
-            <div className="details-header">
-                <h2>{event_date}</h2>
-                <h1>{event.name}</h1>
-                <p>Event Organised by NASA Kenya</p>
-            </div>
-            <div className="map-image">
-                <div>
-                    <img src={event.image} alt="" />
-                </div>
-                <div className="mapouter">
-                    <div className="gmap_canvas">
-                        <iframe
-                            title="Event Location"
-                            id="gmap_canvas"
-                            src={url}
-                            frameBorder="0"
-                            scrolling="no"
-                            marginHeight="0"
-                            marginWidth="0"
-                        ></iframe>
-                    </div>
-                </div>
-            </div>
-            <div className="details-description">
-                <div className='about'>
-                    <h1>About This Event</h1>
-                    <p className='like' onClick={() => handleAddToLikes(event)}>
-                        <i class="fa-regular fa-heart"></i>
-                        <span className='tooltiptext'>Like Event</span>
-                    </p>
-
-                </div>
-                <p>
-                    {event.description}
-                </p>
-
-            </div>
-            <div className="tickets">
-                <div className="date-location">
-                    <p><span>When: </span> {event_date}</p>
-                    <p><span>Where: </span> {event.location}</p>
-                    <p><span>Time: </span> {`${event.start_time} - ${event.end_time}`} EAT</p>
-                </div>
-                {event.price === null &&
-                    <div className='tiko'>
-                        <p><span>Price: </span> Free</p>
-                        <button>Reserve a spot</button>
-                    </div>
-                }
-                {event.price !== null &&
-                    <div className='tiko'>
-                        <p><span>Price: </span>Kshs. {event.price}</p>
-                        <button>Buy Tickets</button>
-                    </div>
-                }
-            </div>
-        </div>
-    )
+        <>
+            {loading ? (
+                <Loading />
+            ) : (
+                <EventItem
+                    user={user}
+                    handleAddToLikes={handleAddToLikes}
+                    event={event}
+                    url={url}
+                />
+            )}
+        </>
+    );
 }
